215eb72818decd54e7807ccfa5ab5e435e1d2c29
dinesh <thedinesh0811@gmail.com>
2024-10-07 23:39:14 +0530

    matrimony test first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,34 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule)
+  },
+  {
+    path: 'my-matches',
+    loadChildren: () => import('./pages/my-matches/my-matches.module').then(m => m.MyMatchesPageModule)
+  },
+  {
+    path: 'daily-recommendations',
+    loadChildren: () => import('./pages/daily-recommendations/daily-recommendations.module').then(m => m.DailyRecommendationsPageModule)
+  },
+  {
+    path: 'profile',
+    loadChildren: () => import('./pages/profile/profile.module').then(m => m.ProfilePageModule)
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ToastService } from './services/toast.service';
+import { StorageService } from './services/storage.service';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
+  entryComponents: [],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    IonicModule.forRoot(),
+    AppRoutingModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ToastService,
+    StorageService
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
